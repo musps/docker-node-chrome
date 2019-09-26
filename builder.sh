2c0c31f9a881b20a4bbe00829ee5a137a5ac3d0c
Sercan YILDIZ <ysercanz@gmail.com>
2019-09-26 13:30:44 +0200

    update

--- builder.sh
+++ builder.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 USER=gnatty
 PKG="node-chrome"
 
@@ -13,6 +15,7 @@ echo "--- Builder ---"
 echo "Package: $PKG"
 echo "Version: $VERSION"
 
+docker login --username $USER --password $DOCKER_PASSWORD
 docker build -t $PKG .
 docker tag $PKG $USER/$PKG:$VERSION
 docker push $USER/$PKG:$VERSION
